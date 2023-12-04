2758e7c98aedb602a22061624025d476a86c72a6
jjd5490 <jamesdudek5@gmail.com>
2023-12-04 17:11:33 -0500

    debug env var

--- src/project/users/client.js
+++ src/project/users/client.js
@@ -8,6 +8,7 @@ export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 console.log("Base API:");
 console.log(BASE_API);
 export const USERS_API = `${BASE_API}/api/users`;
+console.log(USERS_API);
 export const signin = async (credentials) => {
   const response = await request.post(`${USERS_API}/signin`, credentials);
   return response.data;
